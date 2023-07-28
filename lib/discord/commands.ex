6558b0fca2c3b6c79f3d83ae95457e6eb2c3be18
Daniel Oaks <daniel@danieloaks.net>
2023-07-28 10:41:32 +1000

    Move attack response into commands

--- lib/discord/commands.ex
+++ lib/discord/commands.ex
@@ -7,6 +7,7 @@ defmodule MlpdsAttack.Discord.Commands do
 
   alias Nostrum.Api
   alias Nostrum.Constants.ApplicationCommandOptionType, as: OptionType
+  alias Nostrum.Struct.Interaction
 
   defp attack_command() do
     %{
@@ -40,5 +41,77 @@ defmodule MlpdsAttack.Discord.Commands do
 
     # https://kraigie.github.io/nostrum/application_commands.html
     Api.create_guild_application_command(guild_id, attack_command())
+  end
+
+  defp attack_response(attacker, victim, url, filename, message) do
+    Logger.debug("Trying to download #{url}")
+
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        %{
+          # ChannelMessageWithSource
+          type: 4,
+          data: %{
+            content: "<@#{attacker}> attacked <@#{victim}>!\n#{message}",
+            files: [
+              %{
+                name: filename,
+                body: body
+              }
+            ]
+          }
+        }
+
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        Logger.info("404")
+
+        %{
+          # ChannelMessageWithSource
+          type: 4,
+          data: %{
+            content: "Command failed"
+          }
+        }
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        Logger.info("HTTPoison error on grabbing url: #{reason}")
+
+        %{
+          # ChannelMessageWithSource
+          type: 4,
+          data: %{
+            content: "Command failed"
+          }
+        }
+    end
+  end
+
+  def handle_attack(
+         %Interaction{
+           data: %{
+             name: "attack",
+             options: [
+               %{type: 6} = victimOption,
+               %{type: 3} = messageOption,
+               %{type: 11} = mediaOption
+             ],
+             resolved: %{attachments: attachments}
+           }
+         } = interaction
+       ) do
+    Logger.debug(
+      "Processing attack: #{victimOption.value}, #{messageOption.value}, #{mediaOption.value}"
+    )
+
+    Api.create_interaction_response(
+      interaction,
+      attack_response(
+        interaction.user.id,
+        victimOption.value,
+        attachments[mediaOption.value].url,
+        attachments[mediaOption.value].filename,
+        messageOption.value
+      )
+    )
   end
 end
--- lib/discord/consumer.ex
+++ lib/discord/consumer.ex
@@ -6,79 +6,7 @@ defmodule MlpdsAttack.Discord.Consumer do
   require Logger
   use Nostrum.Consumer
 
-  alias Nostrum.Api
   alias Nostrum.Struct.Interaction
-
-  defp attack_response(attacker, victim, url, filename) do
-    Logger.debug("Trying to download #{url}")
-
-    case HTTPoison.get(url) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        %{
-          # ChannelMessageWithSource
-          type: 4,
-          data: %{
-            content: "<@#{attacker}> attacked <@#{victim}>!",
-            files: [
-              %{
-                name: filename,
-                body: body
-              }
-            ]
-          }
-        }
-
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        Logger.info("404")
-
-        %{
-          # ChannelMessageWithSource
-          type: 4,
-          data: %{
-            content: "Command failed"
-          }
-        }
-
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        Logger.info("HTTPoison error on grabbing url: #{reason}")
-
-        %{
-          # ChannelMessageWithSource
-          type: 4,
-          data: %{
-            content: "Command failed"
-          }
-        }
-    end
-  end
-
-  defp handle_attack(
-         %Interaction{
-           data: %{
-             name: "attack",
-             options: [
-               %{type: 6} = victimOption,
-               %{type: 3} = messageOption,
-               %{type: 11} = mediaOption
-             ],
-             resolved: %{attachments: attachments}
-           }
-         } = interaction
-       ) do
-    Logger.debug(
-      "Processing attack: #{victimOption.value}, #{messageOption.value}, #{mediaOption.value}"
-    )
-
-    Api.create_interaction_response(
-      interaction,
-      attack_response(
-        interaction.user.id,
-        victimOption.value,
-        attachments[mediaOption.value].url,
-        attachments[mediaOption.value].filename
-      )
-    )
-  end
 
   def handle_event({:READY, msg, _ws_state}) do
     Logger.info("Discord connection is ready")
@@ -92,7 +20,7 @@ defmodule MlpdsAttack.Discord.Consumer do
         {:INTERACTION_CREATE, %Interaction{data: %{name: "attack"}} = interaction, _ws_state}
       ) do
     Logger.info("Attack found!")
-    handle_attack(interaction)
+    MlpdsAttack.Discord.Commands.handle_attack(interaction)
   end
 
   # Default event handler, if you don't include this, your consumer WILL crash if
